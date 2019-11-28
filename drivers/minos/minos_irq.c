56da509b8489fdae838c32ba4e55290f57608b50
Min Le <lemin9538@gmail.com>
2019-11-28 09:40:20 +0800

    using irq_create_mapping to get new virq for vm
    Signed-off-by: Min Le <lemin9538@gmail.com>

--- drivers/minos/minos_irq.c
+++ drivers/minos/minos_irq.c
@@ -23,27 +23,49 @@
 #include <linux/platform_device.h>
 #include <linux/of.h>
 #include <linux/of_platform.h>
+#include <linux/irqdomain.h>
 
 #define SPI_IRQ_BASE 32
 
-static struct platform_device *mpdev;
+static struct irq_domain *mdomain;
+
+static struct irq_domain *of_find_irq_host(void)
+{
+	int ret;
+	uint32_t phandle;
+	struct device_node *node;
+
+	ret = of_property_read_u32(of_root, "interrupt-parent", &phandle);
+	if (ret || !phandle) {
+		pr_err("failed to get the phandle of the interrupt controller\n");
+		return NULL;
+	}
+
+	node = of_find_node_by_phandle(phandle);
+	if (!node)
+		return NULL;
+
+	return irq_find_host(node);
+}
 
 int get_dynamic_virq(int irq)
 {
-	struct device_node *node;
+	WARN_ONCE(!mdomain, "irq domain not init\n");
 
-	if (!mpdev) {
-		node = of_find_node_by_name(of_root, "vm_fake_device");
-		if (!node)
-			return 0;
-
-		mpdev = of_find_device_by_node(node);
-	}
-
-	if (!mpdev) {
-		pr_err("fake device not init\n");
+	if (!mdomain)
 		return 0;
-	}
 
-	return platform_get_irq(mpdev, irq - SPI_IRQ_BASE);
+	return irq_create_mapping(mdomain, irq);
 }
+
+static int minos_irq_init(void)
+{
+	mdomain = of_find_irq_host();
+	if (!mdomain)
+		pr_err("can not find the root irq domain\n");
+	else
+		pr_info("find irq domain %s\n", mdomain->name);
+
+	return 0;
+}
+arch_initcall(minos_irq_init);
